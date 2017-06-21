58b065f9ba4bc055adef1e62bc86e8d99ba65367
Davo Faulkner <d@davo.co>
2017-06-21 17:15:27 -0500

    Update menus in both activities

--- app/src/main/java/co/davo/habittracker/EditorActivity.java
+++ app/src/main/java/co/davo/habittracker/EditorActivity.java
@@ -2,6 +2,7 @@ package co.davo.habittracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
     }
 }
