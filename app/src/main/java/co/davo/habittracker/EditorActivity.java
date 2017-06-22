08b558a2c33d5d65ab257f33d64b50da6c968f92
Davo Faulkner <d@davo.co>
2017-06-22 00:15:59 -0500

    Delete options menus

--- app/src/main/java/co/davo/habittracker/EditorActivity.java
+++ app/src/main/java/co/davo/habittracker/EditorActivity.java
@@ -1,8 +1,7 @@
 package co.davo.habittracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
+import android.support.v7.app.AppCompatActivity;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -10,13 +9,5 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_editor.xml file.
-        // This adds menu items to the app bar.
-        getMenuInflater().inflate(R.menu.menu_editor, menu);
-        return true;
     }
 }
--- app/src/main/java/co/davo/habittracker/HistoryActivity.java
+++ app/src/main/java/co/davo/habittracker/HistoryActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 public class HistoryActivity extends AppCompatActivity {
@@ -26,27 +24,5 @@ public class HistoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_history, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_delete_all_entries) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
