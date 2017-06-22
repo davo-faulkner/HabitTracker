99c19f699c295682b97a8c8c016ec5d5f8a8564e
Davo Faulkner <d@davo.co>
2017-06-22 13:20:05 -0500

    Clean up EditorActivity.java

--- app/src/main/java/co/davo/habittracker/EditorActivity.java
+++ app/src/main/java/co/davo/habittracker/EditorActivity.java
@@ -30,33 +30,25 @@ public class EditorActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu options from the res/menu/menu_editor.xml file.
-        // This adds menu items to the app bar.
         getMenuInflater().inflate(R.menu.menu_editor, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // User clicked on a menu option in the app bar overflow menu
         switch (item.getItemId()) {
-            // Respond to a click on the "Save" menu option
             case R.id.action_save:
-                // Save pet to database
-                insertPet();
-                // Exit activity
+                insertHabit();
                 finish();
                 return true;
-            // Respond to a click on the "Up" arrow button in the app bar
             case android.R.id.home:
-                // Navigate back to parent activity (CatalogActivity)
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertPet() {
+    private void insertHabit() {
         int weight = Integer.parseInt(weightEditText.getText().toString().trim());
         Date date = Calendar.getInstance().getTime();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM d, yyyy  h:mm a z");
@@ -73,10 +65,8 @@ public class EditorActivity extends AppCompatActivity {
         long newRowId = db.insert(HabitEntry.TABLE_NAME, null, values);
 
         if (newRowId == -1) {
-            // If the row ID is -1, then there was an error with insertion.
             Toast.makeText(this, "Error saving weight", Toast.LENGTH_SHORT).show();
         } else {
-            // Otherwise, the insertion was successful and we can display a toast with the row ID.
             Toast.makeText(this, "Weight saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
         }
     }
