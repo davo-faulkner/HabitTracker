97ec00fc7d3f553ce492678cc32fc494d78a8b76
Davo Faulkner <d@davo.co>
2017-06-22 06:08:18 -0500

    Finish EditorActivity.java

--- app/src/main/java/co/davo/habittracker/EditorActivity.java
+++ app/src/main/java/co/davo/habittracker/EditorActivity.java
@@ -1,11 +1,21 @@
 package co.davo.habittracker;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import co.davo.habittracker.data.HabitContract.HabitEntry;
+import co.davo.habittracker.data.HabitDbHelper;
 
 public class EditorActivity extends AppCompatActivity {
     private EditText weightEditText;
@@ -14,6 +24,8 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        weightEditText = (EditText) findViewById(R.id.weight_editText);
     }
 
     @Override
@@ -45,5 +57,27 @@ public class EditorActivity extends AppCompatActivity {
     }
 
     private void insertPet() {
+        int weight = Integer.parseInt(weightEditText.getText().toString().trim());
+        Date date = Calendar.getInstance().getTime();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM d, yyyy h:mm a z");
+        String dateString = simpleDateFormat.format(date);
+        
+        HabitDbHelper dbHelper = new HabitDbHelper(this);
+
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(HabitEntry.COLUMN_WEIGHT, weight);
+        values.put(HabitEntry.COLUMN_DATE, dateString);
+
+        long newRowId = db.insert(HabitEntry.TABLE_NAME, null, values);
+
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, "Error saving weight", Toast.LENGTH_SHORT).show();
+        } else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, "Weight saved with row id: " + newRowId, Toast.LENGTH_SHORT).show();
+        }
     }
 }
